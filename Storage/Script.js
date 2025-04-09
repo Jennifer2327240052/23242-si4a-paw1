f922eadba2465a943ad585e1c903050a1404fb29
Jennifer2327240052 <jennifer_2327240052@mhs.mdp.ac.id>
2025-04-09 15:10:45 +0700

    push

--- Storage/Script.js
+++ Storage/Script.js
@@ -7,8 +7,10 @@ function simpan(){
     let nama = document.getElementById('nama').value;
     // ambil value dari input
     let keperluan = document.getElementById('keperluan').value;
+    let jk = document.getElementById('jk').value;
     console.log(keperluan);
     console.log(nama);
+    console.log(jk);
 
     // cek local storage kosong
     if(localStorage.getItem('Ls_BukuTamu') == null){
@@ -19,24 +21,41 @@ function simpan(){
 
     }
 
-    
-    data.push({Nama_Pengunjung: nama, Perlu: keperluan}); 
+
+    data.push({Nama_Pengunjung: nama, Perlu: keperluan, jk: jk});
     // masukkan value input nama di dlm array
+
+    localStorage.setItem('Ls_BukuTamu',JSON.stringify(data) );
+    // simpan ke local storage
 
     // kosongkan daftar tamu
     daftar_tamu.innerHTML = '';
     // panggil fungsi tampil
     tampil();
-    
-    localStorage.setItem('Ls_BukuTamu',JSON.stringify(data) );
-    // simpan ke local storage
+
 }
 
 
 function tampil(){
     localStorage.getItem('Ls_BukuTamu') == null ? data = [] : data = JSON.parse(localStorage.getItem('Ls_BukuTamu'));
 
+
+    console.log(data.length);
+
+    document.getElementById('total_tamu').innerHTML = `Total tamu : ${data.length}`; //tampilkan data di console
+
+    let total_laki=0;
+    let total_perempuan=0;
+
     data.forEach((item) => {
+        if(item.jk == 'L'){
+            total_laki++
+        } else if (item.jk == 'P'){
+            total_perempuan++
+        }
+
         daftar_tamu.innerHTML += `<li> ${item.Nama_Pengunjung} - ${item.Perlu} </li>`
     })
+    document.getElementById('tamu_laki').innerHTML = `Total Tamu laki-laki: ${total_laki}`;
+    document.getElementById('tamu_perempuan').innerHTML = `Total Tamu Perempuan: ${total_perempuan}`;
 }
